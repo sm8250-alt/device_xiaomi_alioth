415ac8b455b3a863bd3f35a8123b04566aab8bdf
Aditya R <gh0strider.2k18.reborn@gmail.com>
2023-12-03 18:46:14 +0100

    alioth: Add toggle for Dolby volume leveler Change-Id: I63804773535f290d09724cfc8b1ee58f32ca89ab

--- parts/src/co/aospa/settings/dolby/DolbyUtils.java
+++ parts/src/co/aospa/settings/dolby/DolbyUtils.java
@@ -39,8 +39,6 @@ public final class DolbyUtils {
         mDolbyAtmos = new DolbyAtmos(EFFECT_PRIORITY, 0);
         mDolbyAtmos.setEnabled(mDolbyAtmos.getDsOn());
 
-        // Always disable volume leveler because its broken on AOSP.
-        disableVolumeLeveler();
     }
 
     public static synchronized DolbyUtils getInstance(Context context) {
@@ -74,7 +72,6 @@ public final class DolbyUtils {
         checkEffect();
         Log.d(TAG, "setProfile: " + index);
         mDolbyAtmos.setProfile(index);
-        disableVolumeLeveler();
     }
 
     public int getProfile() {
@@ -96,12 +93,6 @@ public final class DolbyUtils {
     public void resetProfileSpecificSettings() {
         checkEffect();
         mDolbyAtmos.resetProfileSpecificSettings();
-        disableVolumeLeveler();
-    }
-
-    private void disableVolumeLeveler() {
-        Log.d(TAG, "disableVolumeLeveler");
-        mDolbyAtmos.setDapParameterBool(DsParam.VOLUME_LEVELER, false);
     }
 
     public void setPreset(String preset) {
@@ -156,5 +147,17 @@ public final class DolbyUtils {
         int amount = enabled ? mDolbyAtmos.getDapParameterInt(DsParam.STEREO_WIDENING) : 0;
         Log.d(TAG, "getStereoWideningAmount: enabled=" + enabled + " amount=" + amount);
         return amount;
+    }
+
+    public void setVolumeLevelerEnabled(boolean enable) {
+        checkEffect();
+        Log.d(TAG, "setVolumeLevelerEnabled: " + enable);
+        mDolbyAtmos.setDapParameterBool(DsParam.VOLUME_LEVELER, enable);
+    }
+
+    public boolean getVolumeLevelerEnabled() {
+        boolean enabled = mDolbyAtmos.getDapParameterBool(DsParam.VOLUME_LEVELER);
+        Log.d(TAG, "getVolumeLevelerEnabled: " + enabled);
+        return enabled;
     }
 }
